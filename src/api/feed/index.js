4a2bd85ea437a0958cb909127035547ea3566ee6
Gaurav Arora <gauravarora91@yahoo.com>
2017-07-13 02:27:41 +0530

    make it better

---
+++ src/api/feed/index.js
@@ -0,0 +1,117 @@
+import multer from 'multer'
+import { Router } from 'express'
+import { middleware as query } from 'querymen'
+import { middleware as body } from 'bodymen'
+import { token } from '../../services/passport'
+import { create, index, show, update, destroy, updatePhoto } from './controller'
+import { schema } from './model'
+export Feed, { schema } from './model'
+
+const router = new Router()
+const upload = multer({
+  limits: {
+    fileSize: 10 * Math.pow(1024, 2) // 20MB
+  }
+})
+const { type, url, category, text, image, slug } = schema.tree
+
+/**
+ * @api {post} /feeds Create feed
+ * @apiName CreateFeed
+ * @apiGroup Feed
+ * @apiPermission user
+ * @apiParam {String} access_token user access token.
+ * @apiParam type Feed's type.
+ * @apiParam url Feed's url.
+ * @apiParam category Feed's category.
+ * @apiParam text Feed's text.
+ * @apiParam image Feed's image.
+ * @apiParam slug Feed's slug.
+ * @apiSuccess {Object} feed Feed's data.
+ * @apiError {Object} 400 Some parameters may contain invalid values.
+ * @apiError 404 Feed not found.
+ * @apiError 401 user access only.
+ */
+router.post('/',
+  token({ required: true }),
+  body({ type, url, category, text, image, slug }),
+  create)
+
+/**
+ * @api {get} /feeds Retrieve feeds
+ * @apiName RetrieveFeeds
+ * @apiGroup Feed
+ * @apiUse listParams
+ * @apiSuccess {Object[]} feeds List of feeds.
+ * @apiError {Object} 400 Some parameters may contain invalid values.
+ */
+router.get('/',
+  query(),
+  index)
+
+/**
+ * @api {get} /feeds/:id Retrieve feed
+ * @apiName RetrieveFeed
+ * @apiGroup Feed
+ * @apiSuccess {Object} feed Feed's data.
+ * @apiError {Object} 400 Some parameters may contain invalid values.
+ * @apiError 404 Feed not found.
+ */
+router.get('/:id',
+  show)
+
+/**
+ * @api {put} /feeds/:id/image Update feed photo
+ * @apiName UpdateFeedPhoto
+ * @apiGroup Feed
+ * @apiPermission user
+ * @apiParam {String} access_token user access token.
+ * @apiParam data The file.
+ * @apiSuccess {Object} feed Feed's data.
+ * @apiError {Object} 400 Some parameters may contain invalid values.
+ * @apiError 404 Feed not found.
+ * @apiError 401 user access only.
+ */
+router.put('/:id/image',
+  token({ required: true }),
+  upload.single('data'),
+  updatePhoto)
+
+
+/**
+ * @api {put} /feeds/:id Update feed
+ * @apiName UpdateFeed
+ * @apiGroup Feed
+ * @apiPermission user
+ * @apiParam {String} access_token user access token.
+ * @apiParam type Feed's type.
+ * @apiParam url Feed's url.
+ * @apiParam category Feed's category.
+ * @apiParam text Feed's text.
+ * @apiParam image Feed's image.
+ * @apiParam slug Feed's slug.
+ * @apiSuccess {Object} feed Feed's data.
+ * @apiError {Object} 400 Some parameters may contain invalid values.
+ * @apiError 404 Feed not found.
+ * @apiError 401 user access only.
+ */
+router.put('/:id',
+  token({ required: true }),
+  body({ type, url, category, text, image, slug }),
+  update)
+
+/**
+ * @api {delete} /feeds/:id Delete feed
+ * @apiName DeleteFeed
+ * @apiGroup Feed
+ * @apiPermission user
+ * @apiParam {String} access_token user access token.
+ * @apiSuccess (Success 204) 204 No Content.
+ * @apiError 404 Feed not found.
+ * @apiError 401 user access only.
+ */
+router.delete('/:id',
+  token({ required: true }),
+  destroy)
+
+export default router
---
+++ src/api/feed/model.js
@@ -0,0 +1,59 @@
+import mongoose, { Schema } from 'mongoose'
+
+const feedSchema = new Schema({
+  user: {
+    type: Schema.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  type: {
+    type: String
+  },
+  url: {
+    type: String
+  },
+  category: {
+    type: String
+  },
+  text: {
+    type: String
+  },
+  image: {
+    small: String,
+    medium: String,
+    large: String
+  },
+  slug: {
+    type: String
+  }
+}, {
+  timestamps: true
+})
+
+feedSchema.methods = {
+  view (full) {
+    const view = {
+      // simple view
+      id: this.id,
+      user: this.user.view(full),
+      type: this.type,
+      url: this.url,
+      category: this.category,
+      text: this.text,
+      image: this.image,
+      slug: this.slug,
+      createdAt: this.createdAt,
+      updatedAt: this.updatedAt
+    }
+
+    return full ? {
+      ...view
+      // add properties for a full view
+    } : view
+  }
+}
+
+const model = mongoose.model('Feed', feedSchema)
+
+export const schema = model.schema
+export default model
